e179f7c03ac3e3f4c01099f528187ce32faaa736
bboggo <kmt0819@naver.com>
2022-05-24 16:26:51 +0900

    add faceRecognition-v.2

--- face_Recognition_v.2/recognition.py
+++ face_Recognition_v.2/recognition.py
@@ -1,42 +1,65 @@
 import cv2
+import numpy as np
 import os
 
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('/home/pi/myproject/trainer/trainer.yml')
+cascadePath = "/home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# iniciate id counter
+id = 0
+
+# names related to ids: example ==> Marcelo: id=1,  etc
+names = ['None', 'joy', 'Paula', 'bob']
+
+# Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video width
-cam.set(4, 480) # set video height
+cam.set(3, 640)  # set video widht
+cam.set(4, 480)  # set video height
 
-face_detector = cv2.CascadeClassifier('/home/pi/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+# Define min window size to be recognized as a face
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
 
-# For each person, enter one numeric face id
-face_id = input('\n enter user id end press <return> ==>  ')
-
-print("\n [INFO] Initializing face capture. Look the camera and wait ...")
-# Initialize individual sampling face count
-count = 0
-
-while(True):
+while True:
     ret, img = cam.read()
-    img = cv2.flip(img, -1) # flip video image vertically
+    img = cv2.flip(img, -1)  # Flip vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
-        count += 1
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH)),
+    )
 
-        # Save the captured image into the datasets folder
-        cv2.imwrite("/home/pi/myproject/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
-        cv2.imshow('image', img)
+        # Check if confidence is less them 100 ==> "0" is perfect match
+        if (50 < confidence < 100):
+            id = names[id]
+            confidence = "  {0}%".format(round(100 - confidence))
+            print('hi' + names[id])
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+            print('unknown')
 
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('camera', img)
+
+    k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
     if k == 27:
         break
-    elif count >= 30: # Take 30 face sample and stop video
-         break
 
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
-```
